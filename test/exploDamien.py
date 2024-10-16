655107b94f89a05f1c03eaa70e483554db51a921
Damien <damcops1@gmail.com>
2024-10-16 17:05:01 +0200

    pdf2png

--- test/exploDamien.py
+++ test/exploDamien.py
@@ -1,25 +1,26 @@
 import os
 from pdf2image import convert_from_path
 
-rootdir = '/home/servietske/Desktop/Becode/LLM-s-project/data/pdf'
+rootdir = 'data/'
 
-# Walk through all files in the directory
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        # Check if the file has a .pdf extension
         if file.endswith('.pdf'):
-            # Create the full original path
             old_file_path = os.path.join(subdir, file)
             
-            # Convert PDF to images (one image per page)
             images = convert_from_path(old_file_path)
             
-            # Save each page as a PNG with unique filenames based on page number
             for i, image in enumerate(images):
-                # Create a new filename that includes the page number to prevent overwriting
-                new_file_path = os.path.join(subdir, file.replace('.pdf', '.png'))
+                new_file_path = os.path.join(subdir, file.replace('.pdf', f'_page_{i + 1}.png'))
                 
-                # Save the converted image as PNG
                 image.save(new_file_path, 'PNG')
                 
-                print(f"Saved: {new_file_path}")
+                print(new_file_path)
+
+'''import pytesseract
+import cv2
+
+easy_text_path = "/data/image/'BOT_WM hey! Admin Portal FR.png'"
+easy_img = cv2.imread(easy_text_path)
+text = pytesseract.image_to_string(easy_img)
+print(text)'''
